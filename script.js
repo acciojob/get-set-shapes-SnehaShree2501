e58facc1d18542e4cbc93dd2a65164b8dfe814a8
sneha shree <snehashree2501@gmail.com>
2023-04-18 23:24:33 +0530

    getter and setter shapes

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+    width;
+    height;
+    constructor(_width,_height){
+        this.width=_width;
+        this.height=_height;
+    }
+    get width(){
+        return this.width;
+    }
+    get height(){
+        return this.height;
+    }
+    getArea(){
+        return(this.width*this.height);
+    }
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+    getPerimeter(side){
+        return (4*side);
+    }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
